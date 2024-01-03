2164b27c8d9dddf101be78a4b30f8f90a4087a0f
pretzelogical <yingyangyo266@gmail.com>
2024-01-03 00:35:16 -0600

    update to add int support to to_kv

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,11 +3,11 @@ from typing import Tuple
 """ type-annotated function to_kv"""
 
 
-def to_kv(k: str, v: float) -> Tuple[str, float]:
+def to_kv(k: str, v: int | float) -> Tuple[str, float]:
     """
     returns a tuple of a string and a float
     : k(str) : key
-    : v(float) : value
+    : v(int | float) : value
     : return(Tuple[str, float]) : tuple of key and value squared
     """
     return (k, v**2)
