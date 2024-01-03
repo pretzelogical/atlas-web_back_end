4ca39b9f293bd6ea9abb7de2a49731180a6b53e7
pretzelogical <yingyangyo266@gmail.com>
2024-01-03 00:51:42 -0600

    Change | to Union

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import Tuple
-""" type-annotated function to_kv"""
+from typing import Tuple, Union
+""" type-annotated function to_kv """
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     returns a tuple of a string and a float
     : k(str) : key
