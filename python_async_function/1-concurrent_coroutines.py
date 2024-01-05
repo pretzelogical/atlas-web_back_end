c8e0f4e78784b6683dae6397bef60c7225273974
pretzelogical <yingyangyo266@gmail.com>
2024-01-05 09:12:50 -0600

    Add List from typing module

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Async routine created from previous task. """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns n random delays between 0 and max_delay
     in ascending order.
@@ -12,8 +13,8 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     : max_delay(int) : Maximum delay value.
     : return(list) : List of delays.
     """
-    tasks: list[asyncio.Task] = [wait_random(max_delay) for _ in range(n)]
-    delays: list[float] = []
+    tasks: List[asyncio.Task] = [wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = []
     for future in asyncio.as_completed(tasks):
         delay = await future
         delays.append(delay)
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ Task version of wait_n """
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Returns a list of all the delays using a task
     list in ascending order.
@@ -12,8 +13,8 @@ async def task_wait_n(n: int, max_delay: int) -> list[float]:
     : max_delay(int) : maximum delay to create
     : return(list[float]) : list of delays in ascending order.
     """
-    tasks: list[asyncio.Task] = [task_wait_random(max_delay) for _ in range(n)]
-    delays: list[float] = []
+    tasks: List[asyncio.Task] = [task_wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = []
     for future in asyncio.as_completed(tasks):
         delay = await future
         delays.append(delay)
