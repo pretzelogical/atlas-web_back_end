c26b36e382a30aea7cbfea207c72814ebba52e0c
pretzelogical <yingyangyo266@gmail.com>
2024-01-05 09:02:07 -0600

    try to satisfy checker

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> list[float]:
     """
     Returns n random delays between 0 and max_delay
     in ascending order.
@@ -12,8 +12,8 @@ async def wait_n(n: int, max_delay: int) -> list:
     : max_delay(int) : Maximum delay value.
     : return(list) : List of delays.
     """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = []
+    tasks: list[asyncio.Task] = [wait_random(max_delay) for _ in range(n)]
+    delays: list[float] = []
     for future in asyncio.as_completed(tasks):
         delay = await future
         delays.append(delay)
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -12,8 +12,8 @@ async def task_wait_n(n: int, max_delay: int) -> list[float]:
     : max_delay(int) : maximum delay to create
     : return(list[float]) : list of delays in ascending order.
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    delays = []
+    tasks: list[asyncio.Task] = [task_wait_random(max_delay) for _ in range(n)]
+    delays: list[float] = []
     for future in asyncio.as_completed(tasks):
         delay = await future
         delays.append(delay)
