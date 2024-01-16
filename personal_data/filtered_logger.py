1b62ff25d0597ac79945a11b2fe63a948633061a
pretzelogical <yingyangyo266@gmail.com>
2024-01-16 14:06:22 -0600

    brainwormz

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,13 +8,13 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
+                 message: str, separator: str) -> str:
     """
         Filters message so redacted data is replaced with redaction
     """
     for field in fields:
-        message = re.sub(f"{field}=.*?{seperator}",
-                         f"{field}={redaction}{seperator}", message)
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}", message)
     return message
 
 
