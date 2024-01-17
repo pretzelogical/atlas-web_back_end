e6f85d88b99cb59f0777dda15cc26720df8ccfd3
pretzelogical <yingyangyo266@gmail.com>
2024-01-17 13:35:48 -0600

    reformat

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,7 +64,10 @@ class BasicAuth(Auth):
     def current_user(self, request=None) -> TypeVar('User'):
         """ Gets the current user for this request """
         auth_header = self.authorization_header(request)
-        base64_auth_header = self.extract_base64_authorization_header(auth_header)
-        decoded_auth_header = self.decode_base64_authorization_header(base64_auth_header)
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
+        decoded_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
         user_credentials = self.extract_user_credentials(decoded_auth_header)
-        return self.user_object_from_credentials(user_credentials[0], user_credentials[1])
+        return self.user_object_from_credentials(user_credentials[0],
+                                                 user_credentials[1])
