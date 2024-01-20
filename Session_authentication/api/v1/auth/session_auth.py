9d42ebf5cf0a457695fba4df8c4dfe7229b9aec2
pretzelogical <yingyangyo266@gmail.com>
2024-01-20 12:05:45 -0600

    Add user id retreival

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -14,8 +14,18 @@ class SessionAuth(Auth):
             : user_id(str) : the user id to create a session from
             : return(str) : the session id
         """
-        if user_id is None or not isinstance(user_id, str):
+        if not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+            Returns a User ID based on the session_id
+            : session_id(str) : the session id to search for
+            : return(str) : the user id
+        """
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
