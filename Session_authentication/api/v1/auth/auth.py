3e7a0317b2d465bdaac45790b86b031ac0e98af3
pretzelogical <yingyangyo266@gmail.com>
2024-01-20 17:38:26 -0600

    Add Auth.session_cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -2,6 +2,8 @@
 """ Auth module for the API """
 # from flask import request
 from typing import List, TypeVar
+# from flask import request
+import os
 
 
 class Auth:
@@ -26,3 +28,12 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ Documentation!!! """
         return None
+
+    def session_cookie(self, request=None):
+        """
+            Returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        session_name = os.environ.get('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name, None)
