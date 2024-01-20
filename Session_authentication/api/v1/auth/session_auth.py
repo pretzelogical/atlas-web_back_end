47be2c6737800935b0c6fc6e89f81a0bb8ae094b
pretzelogical <yingyangyo266@gmail.com>
2024-01-20 17:58:34 -0600

    add SessionAuth.current_user()

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Authentication module that works on a session basis """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -29,3 +30,10 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """
+            Returns a User instance based on a cookie value
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        return User.get(user_id)
