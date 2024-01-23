759ab7c89b9fb9cfb6031db18760532a08fffc64
pretzelogical <yingyangyo266@gmail.com>
2024-01-23 15:07:21 -0600

    add _generate_uuid

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,11 +4,17 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
     """ Returns a salted hash of the input password """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def _generate_uuid():
+    """ Generate string uuid for auth class """
+    return str(uuid.uuid4())
 
 
 class Auth:
