3ac1f1b2438259a9e6aebfb1f9b74347cdf6caf3
pretzelogical <yingyangyo266@gmail.com>
2024-02-01 09:30:28 -0600

    reset to awesome

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -15,11 +15,6 @@ class Config:
     LANGUAGES = ['en', 'fr']
 
 
-app.config.from_object(Config)
-babel = Babel(app)
-
-
-@babel.localeselector
 def get_locale():
     """ Gets the locale for the current request """
     locale = request.args.get("locale", None)
@@ -28,10 +23,15 @@ def get_locale():
     return locale
 
 
+app.config.from_object(Config)
+babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
+
+
 @app.route('/', strict_slashes=False)
 def root():
     """ Root route """
-    return render_template("4-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
