e5a4b05e5f11244cd07f049759605778957501a3
pretzelogical <yingyangyo266@gmail.com>
2024-02-01 15:09:07 -0600

    add 5, 6, 7

---
+++ i18n/5-app.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" Basic flask app """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from os import getenv
+
+
+class Config:
+    """ Class that holds configuration values for babel """
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ['en', 'fr']
+
+
+def get_user(login_as: int) -> dict:
+    """ Gets user from users """
+    users = {
+        1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+        2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+        3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+        4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+    }
+    if login_as not in users:
+        return None
+    return users[login_as]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale():
+    """ Gets the locale for the current request """
+    locale = request.args.get("locale", None)
+    if locale is None:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return locale
+
+
+@app.before_request
+def before_req():
+    """ Checks for a user and and sets it as a global on flask.g.user """
+    user_id = request.args.get("login_as", None)
+    if user_id is None:
+        g.user = None
+        return
+    g.user = get_user(int(user_id))
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """ Root route """
+    locale = get_locale()
+    return render_template("5-index.html", locale=locale, user=g.user)
+
+
+if __name__ == "__main__":
+    host = getenv("BASIC_HOST", "localhost")
+    port = getenv("BASIC_PORT", "5000")
+    app.run(host, port)
