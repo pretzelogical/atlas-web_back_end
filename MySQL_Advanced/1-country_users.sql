b568dd999f5892eeb0b86a3bfa3a062505fa1368
pretzelogical <yingyangyo266@gmail.com>
2024-02-07 12:37:16 -0600

    Start MySQL_advanced and add 0-2

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates table `users` with additional attributes
+
+CREATE TABLE IF NOT EXISTS `users` (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM("US", "CO", "TN") DEFAULT "US"
+);
