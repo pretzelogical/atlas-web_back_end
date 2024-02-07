0385c9e00739e1f777ce75a4a76747a49ab1f459
pretzelogical <yingyangyo266@gmail.com>
2024-02-07 15:52:39 -0600

    Add 5

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- Resets attribute valid_email when email has been changed
+
+DELIMITER PP
+
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF New.email != Old.email THEN
+        SET New.valid_email = 0;
+    END IF;
+END PP
