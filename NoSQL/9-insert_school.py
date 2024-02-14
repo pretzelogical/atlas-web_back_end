a3f5ab9994f63f5e63c7174f3e23d73a388e43e1
pretzelogical <yingyangyo266@gmail.com>
2024-02-14 11:23:00 -0600

    dont convert to str

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,4 +5,4 @@
 def insert_school(mongo_collection, **kwargs):
     """ Inserts a new_document in a collection based on kwargs """
     res = mongo_collection.insert_one(kwargs)
-    return str(res.inserted_id)
+    return res.inserted_id
