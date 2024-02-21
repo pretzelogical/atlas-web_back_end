a03a4caf5b66af36dcce2e6ee35332f34505742a
pretzelogical <yingyangyo266@gmail.com>
2024-02-20 19:41:21 -0600

    j

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const readline = require("readline");
+const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
-  console.log(`Your name is: ${name}`);
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  process.stdout.write(`Your name is: ${name}\r`);
   rl.close();
-  console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
