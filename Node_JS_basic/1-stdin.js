2a90d86bb2f86b98b9aefeb400b9b1eca8dcfe22
pretzelogical <yingyangyo266@gmail.com>
2024-02-21 11:18:46 -0600

    make 1 use process.stdin.on instead of readline

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  process.stdout.write(`Your name is: ${name}`);
-  rl.close();
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', function(data) {
+  const input = data.toString().trim();
+  console.log(`Your name is: ${input}`);
   console.log('This important software is now closing');
+  process.exit();
 });
