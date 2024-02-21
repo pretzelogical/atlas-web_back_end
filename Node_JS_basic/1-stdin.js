7800b5b0b135ac7c9006bca76f51825828def823
pretzelogical <yingyangyo266@gmail.com>
2024-02-21 11:42:07 -0600

    remove unused param

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,6 +6,6 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${input}`);
 });
 
-process.on('exit', (code) => {
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
