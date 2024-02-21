3eadf56ae8c65ec9a4e18c83105a9eef17e3fb9d
pretzelogical <yingyangyo266@gmail.com>
2024-02-20 18:45:19 -0600

    task 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
-const fs = require("fs/promises")
+const fs = require("fs/promises");
 
+async function countStudents(path) {
+  try {
+    await fs.access(path, fs.constants.R_OK | fs.constants.F_OK);
+  } catch {
+    throw new Error("Cannot load the database");
+  }
+  const database = (await fs.readFile(path)).toString().split("\n");
+  const fields = { CS: [], SWE: [] };
+
+  console.log(`Number of students: ${database.length - 1}`);
+  for (let i = 1; i < database.length; i++) {
+    const entry = database[i].split(",");
+    fields[entry[entry.length - 1]].push(entry[0]);
+  }
+  const studentsCS = fields["CS"].join(", ");
+  const studentsSWE = fields["SWE"].join(", ");
+  console.log(
+    `Number of students in CS: ${fields["CS"].length} List: ${studentsCS}`
+  );
+  console.log(
+    `Number of students in CS: ${fields["SWE"].length} List: ${studentsSWE}`
+  );
+}
+
+module.exports = countStudents;
