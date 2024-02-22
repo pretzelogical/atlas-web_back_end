b0619fe51d4920b1a9466bfee35a651b7239c6b1
pretzelogical <yingyangyo266@gmail.com>
2024-02-22 14:26:24 -0600

    export 6 and 7 apps and start 8

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+const express = require('express');
+const app = express();
+const fs = require('fs').promises;
+const port = 1245;
+
+async function countStudents(path) {
+  try {
+    await fs.access(path, fs.constants.R_OK | fs.constants.F_OK);
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+  const database = ((await fs.readFile(path)).toString().split('\n'))
+    .filter((elem) => elem.trim() !== '');
+  const fields = { CS: [], SWE: [] };
+
+  for (let i = 1; i < database.length; i++) {
+    const entry = database[i].split(',');
+    if (entry[entry.length - 1] in fields) {
+      fields[entry[entry.length - 1]].push(entry[0]);
+    }
+  }
+  const studentsCS = fields.CS.join(', ');
+  const studentsSWE = fields.SWE.join(', ');
+  return [
+    'This is the list of our students',
+    `Number of students: ${database.length - 1}`,
+    `Number of students in CS: ${fields.CS.length}. List: ${studentsCS}`,
+    `Number of students in SWE: ${fields.SWE.length}. List: ${studentsSWE}`
+  ];
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.send((await countStudents(process.argv[2])).join('\n'));
+});
+app.listen(port);
+
+module.exports = app;
+
