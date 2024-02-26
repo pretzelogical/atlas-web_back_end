e2e82c7fc85f0f6d3786cbc17138845698acb4ad
pretzelogical <yingyangyo266@gmail.com>
2024-02-26 15:24:45 -0600

    trick

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,8 @@
-import { assert, expect } from 'chai';
+import { expect } from 'chai';
 import calculateNumber from './2-calcul_chai.js';
+// const assert = require('chai').assert;
+// const expect = require('chai').expect;
+// const calculateNumber = require('./2-calcul_chai.js');
 
 describe('calculateNumber', function () {
   it('should round numbers from float to integer', function () {
