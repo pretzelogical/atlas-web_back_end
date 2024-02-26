43e3e4ace2c8c73a7d3c84bc110c200ca698f9db
pretzelogical <yingyangyo266@gmail.com>
2024-02-26 15:32:38 -0600

    why

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,8 +1,7 @@
 // import { expect } from 'chai';
 // import calculateNumber from './2-calcul_chai.js';
 // const assert = require('chai').assert;
-const chai = require('chai');
-const expect = chai.expect;
+const expect = require('chai').expect;
 const calculateNumber = require('./2-calcul_chai.js');
 
 describe('calculateNumber', function () {
