87fa16cad711d92bb03d1e20f74afc77c64a6e29
pretzelogical <yingyangyo266@gmail.com>
2024-02-26 20:12:36 -0600

    fix arrow funcs

--- unittests_in_js/utils.js
+++ unittests_in_js/utils.js
@@ -3,9 +3,9 @@ const Utils = {
     a = Math.round(a);
     b = Math.round(b);
     const ops = {
-      'SUM': () => a + b,
-      'SUBTRACT': () => a - b,
-      'DIVIDE': () => {
+      'SUM': (a, b) => a + b,
+      'SUBTRACT': (a, b) => a - b,
+      'DIVIDE': (a, b) => {
         if (b <= 0) {
           return 'Error';
         }
