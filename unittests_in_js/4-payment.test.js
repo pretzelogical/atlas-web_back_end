f9e20dd947b151d4c347697594d5216ea8d9f7e5
pretzelogical <yingyangyo266@gmail.com>
2024-02-26 19:57:46 -0600

    task 4

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -4,8 +4,7 @@ const sinon = require('sinon');
 const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', function() {
-  it('Should verify that the math is consistent', function() {
-
+  it('Should verify that the math is consistent', () => {
     sinon.spy(Utils, 'calculateNumber');
 
     expect(sendPaymentRequestToApi(100, 20))
@@ -15,6 +14,16 @@ describe('sendPaymentRequestToApi', function() {
       .to.equal(120);
 
     Utils.calculateNumber.restore();
+  });
 
+  it('Should stub Utils.calculateNumber to always return 10 and spy on console', () => {
+    sinon.stub(Utils, 'calculateNumber').returns(10);
+    sinon.spy(console, 'log');
+
+    expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(10);
+    expect(console.log.calledWith('The total is: 10'));
+
+    Utils.calculateNumber.restore();
+    console.log.restore();
   });
 });
